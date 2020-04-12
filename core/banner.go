2a436fd5a5cf1ee5e3732b684d7d494cb09a7372
hahwul <hahwul@gmail.com>
2020-04-13 01:42:17 +0900

    (#2) update

--- core/banner.go
+++ core/banner.go
@@ -4,7 +4,7 @@ import (
 	"github.com/projectdiscovery/gologger"
 )
 
-// this is DalFox banner
+// Banner is DalFox banner function
 func Banner() {
 	gologger.Printf(`
  __   _   _    ___ _ __ __ |\_/|,,_____,~~
