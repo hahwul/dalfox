2fe1909505e3b128ee6e4a3f6583ee4f3a9941e0
hahwul <hahwul@gmail.com>
2020-04-13 02:33:27 +0900

    update bannder

--- core/banner.go
+++ core/banner.go
@@ -7,11 +7,13 @@ import (
 // Banner is DalFox banner function
 func Banner() {
 	gologger.Printf(`
- __   _   _    ___ _ __ __ |\_/|,,_____,~~
-|  \ / \ | |  | __/ \\ V / (-"-)~~     )'~}}
-| o ) o || |_ | _( o )) (   \o/\ /---~\\ ~}}
-|__/|_n_||___||_| \_//_n_\    _//    _// ~}                          
-	`)
+    _..._
+  .' .::::.   __   _   _    ___ _ __ __ 
+ :  :::::::: |  \ / \ | |  | __/ \\ V / 
+ :  :::::::: | o ) o || |_ | _( o )) (  
+ '. '::::::' |__/|_n_||___||_| \_//_n_\                           
+   '-.::''
+`)
 	gologger.Printf("Parameter Analysis and XSS Scanning tool based on golang\n")
 	gologger.Printf("Finder Of XSS and Dal is the Korean pronunciation of moon. @hahwul\n")
 	gologger.Printf("\n")
