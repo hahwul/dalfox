95806001a8c4f6b0731ab6de2605e36737e17a19
hahwul <hahwul@gmail.com>
2020-04-17 00:54:28 +0900

    Add verifyDOM

---
+++ core/verify.go
@@ -0,0 +1,27 @@
+package core
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"log"
+)
+
+// VerifyDOM is check success inject on code
+func VerifyDOM(body io.ReadCloser, pattern string) bool {
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(body)
+	check := false
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Find the review items
+	doc.Find(pattern).Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		check = true
+	})
+	if check {
+		return true
+	} else {
+		return false
+	}
+}
