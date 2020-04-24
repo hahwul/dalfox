9e6a7d5102988868dd900f809e488689aec944b1
hahwul <hahwul@gmail.com>
2020-04-25 00:45:44 +0900

    #37 Add comments & rename value

--- pkg/optimization/optimization.go
+++ pkg/optimization/optimization.go
@@ -9,13 +9,13 @@ import (
 func MakeRequestQuery(target, param, payload string) string {
 	u, _ := url.Parse(target)
 	data := u.String()
-	temp_p := u.Query()
-	//fmt.Println(temp_p[param])
-	data = strings.Replace(data, param+"="+temp_p[param][0], param+"="+temp_p[param][0]+payload, 1)
-	temp_url, _ := url.Parse(data)
-	temp_q := temp_url.Query()
-	temp_url.RawQuery = temp_q.Encode()
-	return temp_url.String()
+	tempParam := u.Query()
+	//fmt.Println(tempParam[param])
+	data = strings.Replace(data, param+"="+tempParam[param][0], param+"="+tempParam[param][0]+payload, 1)
+	tempURL, _ := url.Parse(data)
+	tempQuery := tempURL.Query()
+	tempURL.RawQuery = tempQuery.Encode()
+	return tempURL.String()
 }
 
 // MakePathQuery is generate http query with path
@@ -27,10 +27,10 @@ func MakePathQuery(target, payload string) string {
 	} else {
 		data = u.Scheme + "://" + u.Hostname() + "/" + u.Path + ";" + payload
 	}
-	temp_url, _ := url.Parse(data)
-	temp_q := temp_url.Query()
-	temp_url.RawQuery = temp_q.Encode()
-	return temp_url.String()
+	tempURL, _ := url.Parse(data)
+	tempQuery := tempURL.Query()
+	tempURL.RawQuery = tempQuery.Encode()
+	return tempURL.String()
 }
 
 // Optimization is remove payload included badchar
