bc3a9e5aceff29084e721119d2e6737ae335999a
hahwul <hahwul@gmail.com>
2020-04-30 17:26:10 +0900

    (Fixed #53) modify file logging format

--- pkg/printing/logger.go
+++ pkg/printing/logger.go
@@ -16,29 +16,53 @@ var (
 // DalLog is log fomatting for DalFox
 func DalLog(level, text string, optionsStr map[string]string) {
 	fdata := text
+	var ftext string
 	if level == "INFO" {
+		if optionsStr["output"] != "" {
+			ftext = "[I] " + text
+		}
 		text = aurora.Blue("[I] ").String() + text
+
 	}
 	if level == "WEAK" {
+		if optionsStr["output"] != "" {
+			ftext = "[W] " + text
+		}
 		text = aurora.Yellow("[W] ").String() + text
+
 	}
 	if level == "VULN" {
+		if optionsStr["output"] != "" {
+			ftext = "[V] " + text
+		}
 		text = aurora.Red("[V] ").String() + text
+
 	}
 	if level == "SYSTEM" {
+		if optionsStr["output"] != "" {
+			ftext = "[*] " + text
+		}
 		text = aurora.White("[*] ").String() + text
+
 	}
 
 	if level == "CODE" {
+		if optionsStr["output"] != "" {
+			ftext = "    " + text
+		}
 		text = aurora.Gray(16-1, "    "+text).String()
 	}
 
 	if level == "ERROR" {
+		if optionsStr["output"] != "" {
+			ftext = "[I] " + text
+		}
 		text = aurora.Yellow("[E] ").String() + text
 	}
 
 	//mutex.Lock()
 	if level == "PRINT" {
+		ftext = "    +> " + text
 		text = "    +> " + text
 		fmt.Println(text)
 	} else {
@@ -47,16 +71,16 @@ func DalLog(level, text string, optionsStr map[string]string) {
 	}
 
 	if optionsStr["output"] != "" {
-		var ftext string
+		var fdtext string
 		if strings.ToLower(optionsStr["format"]) == "json" {
 			escapedFdata := strings.ReplaceAll(fdata, "\"", "\\\"")
-			ftext = "{\"type\":\"" + level + "\",\"text\":\"" + escapedFdata + "\"},"
+			fdtext = "{\"type\":\"" + level + "\",\"text\":\"" + escapedFdata + "\"},"
 		} else if strings.ToLower(optionsStr["format"]) == "xml" {
 			escapedFdata := strings.ReplaceAll(fdata, "<", "&lt;")
 			escapedFdata = strings.ReplaceAll(escapedFdata, ">", "&gt;")
-			ftext = "<log><type>" + level + "</type><value>" + escapedFdata + "</value></log>"
+			fdtext = "<log><type>" + level + "</type><value>" + escapedFdata + "</value></log>"
 		} else { // format: txt or any case
-			ftext = text
+			fdtext = ftext
 		}
 		f, err := os.OpenFile(optionsStr["output"],
 			os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -64,7 +88,7 @@ func DalLog(level, text string, optionsStr map[string]string) {
 			fmt.Fprintln(os.Stderr, "output file error (file)")
 		}
 		defer f.Close()
-		if _, err := f.WriteString(ftext + "\n"); err != nil {
+		if _, err := f.WriteString(fdtext + "\n"); err != nil {
 			fmt.Fprintln(os.Stderr, "output file error (write)")
 		}
 	}
--- pkg/printing/version.go
+++ pkg/printing/version.go
@@ -1,3 +1,3 @@
 package printing
 
-const VERSION = "v1.0.2-dev"
+const VERSION = "v1.0.3-dev"
