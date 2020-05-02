5d53bb5d896cc65e519d93fd8eaba54b0ee85d4c
hahwul <hahwul@gmail.com>
2020-05-03 00:41:02 +0900

    #58 Add grepping functions

---
+++ pkg/scanning/grep.go
@@ -0,0 +1,49 @@
+package scanning
+
+import "regexp"
+
+// Grepping is function for checking pattern
+func Grepping(data, regex string) []string {
+	byteData := []byte(data)
+	var bodySlice []string
+	var pattern = regexp.MustCompile(regex)
+	result := pattern.FindAllIndex(byteData, -1)
+	_ = result
+
+	for _, v := range result {
+		bodySlice = append(bodySlice, data[v[0]:v[1]])
+	}
+	return bodySlice
+}
+
+// builtinGrep is dalfox build-in grep pattern
+func builtinGrep(data string) map[string][]string {
+	// "pattern name":["list of grep"]
+	result := make(map[string][]string)
+	// "pattern name":"regex"
+	pattern := map[string]string{
+		"dalfox_ssti": "",
+	}
+
+	for k, v := range pattern {
+		resultArr := Grepping(data, v)
+		if len(resultArr) > 0 {
+			result[k] = resultArr
+		}
+	}
+
+	return result
+}
+
+// customGrep is user custom grep pattern
+func customGrep(data string, pattern map[string]string) map[string][]string {
+	// "pattern name":""
+	result := make(map[string][]string)
+	for k, v := range pattern {
+		resultArr := Grepping(data, v)
+		if len(resultArr) > 0 {
+			result[k] = resultArr
+		}
+	}
+	return result
+}
