17ad90f8eb90ffdf3c88258f41f84bff0d350152
hahwul <hahwul@gmail.com>
2020-06-03 01:49:14 +0900

    #76 multicasting function

---
+++ pkg/multicasting/multicast.go
@@ -0,0 +1,16 @@
+package multicasting
+
+import "net/url"
+
+// makeTargetSlice is make slice for multicast option
+func makeTargetSlice(targets []string) map[string][]string {
+	result := make(map[string][]string)
+	for _, target := range targets {
+		t, err := url.Parse(target)
+		if err == nil {
+			hostname := t.Hostname()
+			result[hostname] = append(result[hostname], target)
+		}
+	}
+	return result
+}
