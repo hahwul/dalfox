cad6d2d5dc7e211368f36aabb3e569837ed0d27d
hahwul <hahwul@gmail.com>
2021-04-01 23:58:40 +0900

    (#215) only redirect check from bav request

--- pkg/scanning/scan.go
+++ pkg/scanning/scan.go
@@ -942,7 +942,7 @@ func SendReq(req *http.Request, payload string, options model.Options) (string, 
 	}
 
 	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		if !options.NoBAV {
+		if !options.NoBAV && (payload == "toOpenRedirecting") {
 			if strings.Contains(req.URL.Host, "google.com") {
 				printing.DalLog("GREP", "Found Open Redirect. Payload: "+via[0].URL.String(), options)
 				if options.FoundAction != "" {
