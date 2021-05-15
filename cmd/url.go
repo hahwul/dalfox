dd67958a9016ca797d016e7738265798801c9e01
hahwul <hahwul@gmail.com>
2021-05-15 23:14:57 +0900

    remove comments

--- cmd/url.go
+++ cmd/url.go
@@ -25,14 +25,4 @@ var urlCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(urlCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// urlCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// urlCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
