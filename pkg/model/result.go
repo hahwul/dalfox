addc0ced3547a734d6174ccf75412f114a960108
hahwul <hahwul@gmail.com>
2021-11-17 01:02:00 +0900

    (#310) Add json object

--- pkg/model/result.go
+++ pkg/model/result.go
@@ -6,16 +6,16 @@ import (
 
 // PoC is PoC struct for Result
 type PoC struct {
-	Type   string
-	Method string
-	Data   string
+	Type   string `json:"type"`
+	Method string `json:"method"`
+	Data   string `json:"data"`
 }
 
 // Result is struct for library and cli application
 type Result struct {
-	Logs      []string
-	PoCs      []PoC
-	Duration  time.Duration
-	StartTime time.Time
-	EndTime   time.Time
+	Logs      []string      `json:"logs"`
+	PoCs      []PoC         `json:"pocs"`
+	Duration  time.Duration `json:"duration"`
+	StartTime time.Time     `json:"start_time"`
+	EndTime   time.Time     `json:"end_time"`
 }
