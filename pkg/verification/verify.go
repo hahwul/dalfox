dd6f561b6e2212cee35f040a0273aff2fbbc0dc1
HAHWUL <hahwul@gmail.com>
2024-12-04 23:59:41 +0900

    Refactor verification functions: improve readability and simplify logic for reflection checks
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- pkg/verification/verify.go
+++ pkg/verification/verify.go
@@ -2,57 +2,35 @@ package verification
 
 import (
 	"fmt"
-	"io/ioutil"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-// VerifyReflectionWithLine is check reflected param for mining
+// VerifyReflectionWithLine checks reflected param for mining
 func VerifyReflectionWithLine(body, payload string) (bool, int) {
 	bodyArray := strings.Split(body, "\n")
-	count := 0
 	for l, v := range bodyArray {
 		if strings.Contains(v, payload) {
-			count = count + l + 1
+			return true, l + 1
 		}
-	}
-	if count != 0 {
-		return true, count
 	}
 	return false, 0
 }
 
-// VerifyReflection is check reflected param for xss and mining
+// VerifyReflection checks reflected param for xss and mining
 func VerifyReflection(body, payload string) bool {
-	if strings.Contains(body, payload) {
-		return true
-	}
-	return false
+	return strings.Contains(body, payload)
 }
 
-// VerifyDOM is check success inject on code
-func VerifyDOM(s string) bool { //(body io.ReadCloser) bool {
-
-	body := ioutil.NopCloser(strings.NewReader(s)) // r type is io.ReadCloser
-	defer body.Close()
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(body)
-	check := false
+// VerifyDOM checks success inject on code
+func VerifyDOM(s string) bool {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(s))
 	if err != nil {
 		fmt.Println(err)
 		return false
 	}
-	// Find the review items
-	doc.Find(".dalfox").Each(func(i int, s *goquery.Selection) {
-		check = true
-	})
-	if !check {
-		doc.Find("#dalfox").Each(func(i int, s *goquery.Selection) {
-			// For each item found, get the band and title
-			check = true
-		})
-	}
-	return check
+
+	// Check for elements with class "dalfox" or id "dalfox"
+	return doc.Find(".dalfox").Length() > 0 || doc.Find("#dalfox").Length() > 0
 }
