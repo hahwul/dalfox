45a40d08c6236719ce1616f0564454bf11521552
HAHWUL <hahwul@gmail.com>
2024-12-05 23:15:51 +0900

    Refactor SetPayloadValue: streamline payload processing and enhance clarity with switch-case structure
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- pkg/optimization/replace.go
+++ pkg/optimization/replace.go
@@ -6,17 +6,19 @@ import (
 	model "github.com/hahwul/dalfox/v2/pkg/model"
 )
 
-// SetPayloadValue is change alert/prompt/conrifm value and type
+// SetPayloadValue is change alert/prompt/confirm value and type
 func SetPayloadValue(payloads []string, options model.Options) []string {
 	var result []string
 	catype := strings.Split(options.CustomAlertType, ",")
-	for _,payload := range payloads {
-		for _,k := range catype {
-			if k == "none" {
-				temp := strings.ReplaceAll(payload, "DALFOX_ALERT_VALUE", options.CustomAlertValue)
+	for _, payload := range payloads {
+		for _, k := range catype {
+			var temp string
+			switch k {
+			case "none":
+				temp = strings.ReplaceAll(payload, "DALFOX_ALERT_VALUE", options.CustomAlertValue)
 				result = append(result, temp)
-			} else if k == "str" {
-				temp := strings.ReplaceAll(payload, "DALFOX_ALERT_VALUE", "\""+options.CustomAlertValue+"\"")
+			case "str":
+				temp = strings.ReplaceAll(payload, "DALFOX_ALERT_VALUE", "\""+options.CustomAlertValue+"\"")
 				result = append(result, temp)
 				temp = strings.ReplaceAll(payload, "DALFOX_ALERT_VALUE", "'"+options.CustomAlertValue+"'")
 				result = append(result, temp)
