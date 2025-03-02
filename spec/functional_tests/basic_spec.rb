50ec49a5cc59f9461d6940d31b811277e2c1ec57
HAHWUL <hahwul@gmail.com>
2025-03-02 23:42:21 +0900

    Add test for help URL command in basic_spec.rb

--- spec/functional_tests/basic_spec.rb
+++ spec/functional_tests/basic_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe 'Basic Run', type: :aruba do
     expect(last_command_started).to be_successfully_executed
   end
 
+  it 'prints the help url mode' do 
+    run_command("#{@binary_path} help url")
+    expect(last_command_started).to have_output(/Usage:/)
+    expect(last_command_started).to be_successfully_executed
+  end
+
   it 'returns an error for unknown commands' do
     run_command("#{@binary_path} invalid-command")
     expect(last_command_started).to have_output(/unknown command/i)
