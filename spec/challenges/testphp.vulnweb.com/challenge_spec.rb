268628bc1e6470cd5d990a0cbcd9e177438d5de6
HAHWUL <hahwul@gmail.com>
2025-03-21 14:16:56 +0900

    refactor: update test descriptions for clarity in challenge_spec.rb

--- spec/challenges/testphp.vulnweb.com/challenge_spec.rb
+++ spec/challenges/testphp.vulnweb.com/challenge_spec.rb
@@ -7,39 +7,39 @@ RSpec.describe 'testphp.vulnweb.com', type: :aruba do
     @binary_path = File.expand_path('../../../dalfox', __dir__)
   end
 
-  it 'listproducts.php - 1' do
+  it 'listproducts.php - cat param' do
     run_command("#{@binary_path} url 'http://testphp.vulnweb.com/listproducts.php?cat='")
-    expect(last_command_started).to have_output(/\[V\]/)
+    expect(last_command_started).to have_output(/\\[V\\]/)
     expect(last_command_started).to be_successfully_executed
   end
 
-  it 'listproducts.php - 1' do
+  it 'listproducts.php - artist param' do
     run_command("#{@binary_path} url 'http://testphp.vulnweb.com/listproducts.php?artist='")
-    expect(last_command_started).to have_output(/\[V\]/)
+    expect(last_command_started).to have_output(/\\[V\\]/)
     expect(last_command_started).to be_successfully_executed
   end
 
-  it 'hpp - 1' do
+  it 'hpp - pp param' do
     run_command("#{@binary_path} url 'http://testphp.vulnweb.com/hpp/?pp='")
-    expect(last_command_started).to have_output(/\[V\]/)
+    expect(last_command_started).to have_output(/\\[V\\]/)
     expect(last_command_started).to be_successfully_executed
   end
 
-  it 'params.php - 1' do
+  it 'params.php - p param' do
     run_command("#{@binary_path} url 'http://testphp.vulnweb.com/hpp/params.php?p='")
-    expect(last_command_started).to have_output(/\[V\]/)
+    expect(last_command_started).to have_output(/\\[V\\]/)
     expect(last_command_started).to be_successfully_executed
   end
 
-  it 'search.php - 1' do
+  it 'search.php - searchFor body param' do
     run_command("#{@binary_path} url 'http://testphp.vulnweb.com/search.php' -d 'searchFor=' -X POST")
-    expect(last_command_started).to have_output(/\[V\]/)
+    expect(last_command_started).to have_output(/\\[V\\]/)
     expect(last_command_started).to be_successfully_executed
   end
 
-  it 'guestbook.php - 1' do
+  it 'guestbook.php - name body param' do
     run_command("#{@binary_path} url 'http://testphp.vulnweb.com/guestbook.php' -d 'name=' -X POST")
-    expect(last_command_started).to have_output(/\[V\]/)
+    expect(last_command_started).to have_output(/\\[V\\]/)
     expect(last_command_started).to be_successfully_executed
   end
 end
