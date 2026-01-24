d134dc82527fa84f4fbad3c2e384f0fbf92204c2
Houssam Miliani <149241633+SecHoussam@users.noreply.github.com>
2026-01-24 15:55:20 +0100

    add Dropbox Access Token

--- pkg/scanning/grep.go
+++ pkg/scanning/grep.go
@@ -68,6 +68,7 @@ var builtinPatterns = map[string]string{
 	"dalfox-Linear-API-Key":        "lin_api_[a-zA-Z0-9]{40}",
 	"dalfox-DigitalOcean-Token":    "dop_v1_[a-z0-9]{64}",
 	"dalfox-Asana-Access-Token":    "0/[0-9a-z]{32}",
+	"dalfox-Dropbox-Access-Token":  "sl.[A-Za-z0-9_-]{20,100}",
 	"dalfox-error-mysql":           "(SQL syntax.*MySQL|Warning.*mysql_.*|MySqlException \\(0x|valid MySQL result|check the manual that corresponds to your (MySQL|MariaDB) server version|MySqlClient\\.|com\\.mysql\\.jdbc\\.exceptions)",
 	"dalfox-error-postgresql":      "(PostgreSQL.*ERROR|Warning.*\\Wpg_.*|valid PostgreSQL result|Npgsql\\.|PG::SyntaxError:|org\\.postgresql\\.util\\.PSQLException|ERROR:\\s\\ssyntax error at or near)",
 	"dalfox-error-mssql":           "(Driver.* SQL[\\-\\_\\ ]*Server|OLE DB.* SQL Server|\bSQL Server.*Driver|Warning.*mssql_.*|\bSQL Server.*[0-9a-fA-F]{8}|[\\s\\S]Exception.*\\WSystem\\.Data\\.SqlClient\\.|[\\s\\S]Exception.*\\WRoadhouse\\.Cms\\.|Microsoft SQL Native Client.*[0-9a-fA-F]{8})",
